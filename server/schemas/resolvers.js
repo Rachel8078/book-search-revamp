48ad45d015ba3c48f00e38538c782beef1953fbc
Rachel Marriott <69021829+Rachel8078@users.noreply.github.com>
2022-05-15 22:07:42 -0600

    incorporate JWT into typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,11 +27,25 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
 
-      return user;
+      return { token, user };
     },
-    login: async() => {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+    
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+    
+      const correctPw = await user.isCorrectPassword(password);
+    
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
+      const token = signToken(user);
+      return { token, user };
     }
   }
 
